aebda1acee1cc4468b4911bc612a6caa47b2a454
AlisonLikoskiN <alison.likoskineves@gmail.com>
2025-05-09 17:25:33 -0300

    lab5/steamTables.py

--- lab5/steamTables.py
+++ lab5/steamTables.py
@@ -24,6 +24,7 @@ import numpy as np
 temperatures = np.linspace(10, 100, 10)
 pressures = [1.227, 2.339, 4.246, 7.384, 12.350, 19.941, 31.19, 47.39, 70.14, 101.3] # KPa
 CpTable = [41.99, 83.94, 125.77, 167.53, 209.30, 251.09, 292.93, 334.84, 376.82, 418.91]
+RhoLiqTable = [999.69, 998.19, 995.61, 992.17, 987.99, 983.16, 977.75, 971.79, 965.33, 954.75]
 
 for i in range(len(pressures)):
     pressures[i] = pressures[i] * 1000 # Convert to Pa
@@ -41,6 +42,7 @@ draw()
 
 # Printing the values of Cp(T,P) from tables vs Thermo values
 print("Cp(T,P) from tables vs Thermo values")
+print("___"*30)
 print("T(C)   | P(Pa)       | Cp(T,P) from tables  | Cp(T,P) from Thermo")
 print("___"*30)
 for i in range(len(temperatures)):
@@ -48,6 +50,19 @@ for i in range(len(temperatures)):
     print(f"{temperatures[i]}   | {pressures[i]}      | {CpTable[i]}               | {CpWater}")
 print("___"*30)
 
+# Printing the values of RhoLiquid(T,P) from tables vs Thermo values
+print("RhoLiquid(T,P) from tables vs Thermo values")
+print("___"*30)
+print("T(C)   | P(Pa)       | RhoLiquid(T,P) from tables  | RhoLiquid(T,P) from Thermo")
+print("___"*30)
+for i in range(len(temperatures)):
+    RhoWater = Chemical("Water", T=temperatures[i], P=pressures[i]).rhol
+    print(f"{temperatures[i]}   | {pressures[i]}      | {RhoLiqTable[i]}               | {RhoWater}")
+print("___"*30)
 
+"""
+Is possible to see that the values of thermos and the official tables are close, but there's a difference.
+This difference need to be analysed and take in consideration depending of the application.
+"""
 
 
